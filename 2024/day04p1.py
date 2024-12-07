cd93dc36262a36a3f991d2c36860a2000fc764c8
Nicolas <nicolas.docolas1804@gmail.com>
2024-12-07 11:29:56 -0300

    optimizing

--- 2024/day04p1.py
+++ 2024/day04p1.py
@@ -10,20 +10,21 @@ dRow = [0, 1, 1, 1, 0, -1, -1, -1]
 dCol = [1, 1, 0, -1, -1, -1, 0, 1]
 
 def check_sonar(i, j):
-    global total
-    for dir in range(8):
-        word = matrix[i][j]
-        row = i + dRow[dir]
-        col = j + dCol[dir]
-        for _ in range(3):
-            if 0 <= row < rows and 0 <= col < cols:
-                word += matrix[row][col]
-                row += dRow[dir]
-                col += dCol[dir]
-            else:
-                break
-        if(word=="XMAS"):
-            total += 1
+    if(matrix[i][j] == "X"):
+        global total
+        for dir in range(8):
+            word = matrix[i][j]
+            row = i + dRow[dir]
+            col = j + dCol[dir]
+            for _ in range(3):
+                if 0 <= row < rows and 0 <= col < cols:
+                    word += matrix[row][col]
+                    row += dRow[dir]
+                    col += dCol[dir]
+                else:
+                    break
+            if(word=="XMAS"):
+                total += 1
 
 for i in range(rows):
     for j in range(cols):
