fa7a2ebce20503bdf57fb05e31b17dd68479a9a5
Nicolas Docolas <nicolas.docolas@hp.com>
2024-12-21 17:09:38 -0300

    day 8

---
+++ 2024/day08p2.py
@@ -0,0 +1,31 @@
+grid = [line.strip() for line in open(0)]
+
+rows = len(grid)
+cols = len(grid[0])
+
+antennas = {}
+
+for r, row in enumerate(grid):
+    for c, char in enumerate(row):
+        if char != ".":
+            if char not in antennas: antennas[char] = []
+            antennas[char].append((r, c))
+
+antinodes = set()
+
+for array in antennas.values():
+    for i in range(len(array)):
+        for j in range(len(array)):
+            if i == j: continue
+            r1, c1 = array[i]
+            r2, c2 = array[j]
+            dr = r2 - r1
+            dc = c2 - c1
+            r = r1
+            c = c1
+            while 0 <= r < rows and 0 <= c < cols:
+                antinodes.add((r, c))
+                r += dr
+                c += dc
+
+print(len(antinodes))
